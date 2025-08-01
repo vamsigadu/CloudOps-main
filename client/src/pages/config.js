d8cc2b1d929e4a3589f1914b1f72dedec9f29ea3
vamsigadu <gaduvamsi4321@gmail.com>
2025-08-02 00:34:38 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -3,6 +3,6 @@ const API_BASE_URL = "http://gadu.vamsi";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
+//const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
 
